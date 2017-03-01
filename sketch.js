42663623577c71d91e3e3dfe338226f47c75bae7
Dyamond <dmcdowney@wlapcs.org>
2017-03-01 19:05:30 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup(){
+    createCanvas(600,400);
+    background(100,100,100);
+}
+
+function draw(){
+    line(35, 80, 400, 200);
+    
+    fill(255,0,0);
+   rect(290,100,200,250); 
+   
+   fill(0,0,255);
+   ellipse(50,250,40,80);
+   
+   
+}
