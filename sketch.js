b1c1055ade909ee62e943e205431577928444bbf
Dyamond <dmcdowney@wlapcs.org>
2017-03-02 18:32:24 +0000

    added more lines and shapes

--- sketch.js
+++ sketch.js
@@ -1,16 +1,34 @@
 function setup(){
     createCanvas(600,400);
-    background(100,100,100);
+    background(66,244,217);
 }
 
 function draw(){
     line(35, 80, 400, 200);
     
-    fill(255,0,0);
-   rect(290,100,200,250); 
+   stroke(255,255,255);
+   fill(255,0,0);
+   rect(290,100,250,250); 
    
+   stroke(0,0,0);
+   strokeWeight(4);
    fill(0,0,255);
    ellipse(50,250,40,80);
    
+   line(100,35,60,100);
    
+   line(400,400,400,300);
+   
+   ellipse(10,50,30,90);
+   
+   ellipse(90,35,50,60);
+   
+   line(50,90,230,100)
+   
+   rect(150,50,200,200)
 }
+
+//draw me 2 ellipses
+//draw me 2 rectangles
+//draw me 4 lines (1 with a positive slope, 1 with a neg slope,)
+//1 with a slope of 0, and 1 with a slope of undefined.
